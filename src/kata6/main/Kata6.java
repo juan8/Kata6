9b5802acc60467dece68299bc3bc45d33e277a6f
Entrar <Entrar@APD-L13>
2017-12-01 12:27:40 +0000

    Kata6 - versión 3

--- src/kata6/main/Kata6.java
+++ src/kata6/main/Kata6.java
@@ -1,34 +1,41 @@
 package kata6.main;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import kata6.model.Histogram;
 import kata6.model.Mail;
+import kata6.model.Person;
 import kata6.view.Attribute;
+import kata6.view.DataBaseList;
 import kata6.view.HistogramDisplay;
 import kata6.view.HistogramBuilder;
 import kata6.view.MailListReader;
 
 public class Kata6 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         Kata6 kata6 = new Kata6();
         kata6.execute();
     }
     
     private HistogramBuilder<Mail> builder;
     private List<Mail> listMail;
+    private List<Person> people;
+    private HistogramBuilder<Person> builderPerson;
     
-    private void execute() throws IOException {
+    private void execute() throws IOException, ClassNotFoundException, SQLException {
         input();
         process();
         output();
     }
     
     
-    private void input() throws IOException {
+    private void input() throws IOException, ClassNotFoundException, SQLException {
         String nameFile="emails.txt";
         listMail = MailListReader.read(nameFile);
+        people = new DataBaseList().read();
+        builderPerson = new HistogramBuilder<>(people);
     }
     
     
@@ -56,6 +63,14 @@ public class Kata6 {
         
         new HistogramDisplay(letters,"Primer Caracter").execute();
         
+        Histogram<Character> gender = builderPerson.build(new Attribute<Person, Character>() {
+            @Override
+            public Character get(Person item) {
+                return item.getGender();
+            }
+        });
+        
+        new HistogramDisplay(gender,"Gender").execute();
     }
     
 }
